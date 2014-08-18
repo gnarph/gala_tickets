493115db9dab7f6e35b3c5a9745bcf44f5b61d53
Gord Larson <gordlarson@gmail.com>
2014-08-17 21:10:35 -0700

    define some models

--- usermodels.py
+++ usermodels.py
@@ -1,4 +1,27 @@
 from google.appengine.ext import db
 
 class Seat(object):
-    pass
+    """
+    A seat
+    """
+    name = db.StringProperty()
+    position_x = db.IntegerProperty()
+    position_y = db.IntegerProperty()
+
+class Patron(object):
+    """
+    A person
+    """
+    name = db.StringProperty()
+    email = db.StringProperty()
+    phone = db.StringProperty()
+    member = db.BooleanProperty()
+
+class PatronSeat(object):
+    """
+    Seat that a person has
+    """
+    seat = db.ReferenceProperty(Seat)
+    patron = db.ReferenceProperty(Patron)
+    start = db.DateProperty()
+    end = db.DateProperty()
